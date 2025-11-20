5fb3ce400f0752acc6d22c324de1db5b85e7c39e
Shwetakankanwadi <shwetakankanwadi878@gmail.com>
2025-11-20 15:14:03 +0530

    Update student.py

--- student.py
+++ student.py
@@ -1,16 +1,10 @@
 import sys
-if len(sys.argv)!=3:
-  print("Usage: python student.py <name> <roll>")
+if len(sys.argv) !=3:
+  print("usage: python student.py<name> <roll>")
   sys.exit(1)
-  
-script_name=sys.arvg[0]
-name=sys.avrg[1]
-rollno=sys.avrg[2]
-
-print("Script name:",script_name)
+script_name=sys.argv[0]
+name=sys.argv[1]
+rollno= sys.argv[2]
+print("script name:",script_name)
 print("student name:",name)
 print("roll number:",rollno)
-
-
-
-
