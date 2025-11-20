d294d41fad0bc2f707190c3b78c500dda8b965c4
Shwetakankanwadi <shwetakankanwadi878@gmail.com>
2025-11-20 14:23:49 +0530

    Create student.py

---
+++ student.py
@@ -0,0 +1,16 @@
+import sys
+if len(sys.argv)!=3:
+  print("usage: python student.py <name> roll")
+sys.exit(1)
+
+
+script_name=sys.argv[0]
+name=sys.argv[1]
+rollno=sys.argv[2]
+
+print("Script name :", script_name)
+print("student name :", name)
+print("roll number :", rollno)
+
+
+
